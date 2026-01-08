77d364bdfb2111b743a00d09bffca4125aea4d90
Shenghao Ding <shenghao.ding@yetanother.ai>
2026-01-08 13:37:10 +0800

    fixes

--- gdcef_helper/src/utils.rs
+++ gdcef_helper/src/utils.rs
@@ -1,7 +1,10 @@
 #[cfg(target_os = "macos")]
+use std::{io::Error, path::PathBuf};
+
+
+#[cfg(target_os = "macos")]
 pub fn get_framework_path() -> Result<PathBuf, Error> {
     use process_path::get_executable_path;
-    use std::{io::Error, path::PathBuf};
 
     let dylib_path = get_executable_path().unwrap();
 
